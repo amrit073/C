e62196dd59aefdc3b4a0f099c48b255dfe8cca59
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-12-21 09:07:04 +0545

    Iei3
    Uej

--- matrix.cpp
+++ matrix.cpp
@@ -27,6 +27,10 @@ int main(){
 // int main()
 // {
 
+
+
+I
+
 //     int matrix1[3][3]={{1, 2}, {1,1},{2,3}}, matrix2[3][3]={{1,0},{2,1},{3,1}}, matrix_sum[3][3], matrix_product[3][3];
     // printf("Enter the value: ");
     // for (int i = 0; i < 3; i++)
